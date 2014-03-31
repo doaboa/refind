1c3ad16ae37cd97cc1e62b8b19663a7ace9239a5
Doa <doajafri@gmail.com>
2014-03-31 18:00:38 -0400

    added jquery scroll

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -11,3 +11,18 @@ function sizeContent() {
     var newHeightSize =  $("html").height() + "px";
     $(".hCtrl").css("height", newHeightSize);
 }
+
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
